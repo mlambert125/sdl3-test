355a52ec7ef5556d27b25fcfc4b2025570fe4f4e
Michael Lambert <mlambert125@live.com>
2025-03-26 13:34:34 -0400

    Removing comment headers

--- include/game-state.h
+++ include/game-state.h
@@ -10,36 +10,11 @@ constexpr int TILE_HILLS = 2;
 constexpr int TILE_MOUNTAINS = 3;
 constexpr int TILE_WATER = 4;
 
-/**
- * Global game state
- */
 typedef struct GlobalGameState {
-    /**
-     * The global SDL renderer.
-     */
     TTF_Font *fontDejaVuSans;
-
-    /**
-     * Tile textures
-     */
     SDL_Texture* textureTiles[5];
-
-    /**
-     * The map script
-     */
     char mapScript[255];
 } GlobalGameState;
 
-/**
- * Create a global game state
- *
- * @return GlobalGameState The global game state
- */
 GlobalGameState globalGameState_create(SDL_Renderer *renderer);
-
-/**
- * Free a global game state
- *
- * @param globalGameState The global game state
- */
 void globalGameState_free(GlobalGameState globalGameState);
--- include/scene-map.h
+++ include/scene-map.h
@@ -4,71 +4,15 @@
 #include <lua.h>
 #include "scene.h"
 
-/**
- * Map scene data
- */
 typedef struct SceneMapData {
-    /**
-     * The Lua state
-     */
     lua_State *L;
-
-    /**
-     * The map
-     */
     int map[500][500];
-
-    /**
-     * The player x position
-     */
     int playerX;
-
-    /**
-     * The player y position
-     */
     int playerY;
 } SceneMapData;
 
-/**
- * Create a map scene
- *
- * @return Scene The map scene
- */
 Scene scene_map_create();
-
-/**
- * Initialize the map scene
- *
- * @param self The map scene
- * @param globalGameState The global game state
- * @param renderer The SDL renderer
- */
 void scene_map_init(Scene *self, GlobalGameState *globalGameState, SDL_Renderer *renderer);
-
-/**
- * Update the map scene
- *
- * @param self The map scene
- * @param globalGameState The global game state
- * @param renderer The SDL renderer
- * @return SceneUpdateResult The scene update result
- */
 SceneUpdateResult scene_map_update(Scene *self, GlobalGameState *globalGameState, SDL_Renderer *renderer);
-
-/**
- * Draw the map scene
- *
- * @param self The map scene
- * @param globalGameState The global game state
- * @param renderer The SDL renderer
- */
 void scene_map_draw(Scene *self, GlobalGameState *globalGameState, SDL_Renderer *renderer);
-
-/**
- * Destroy the map scene
- *
- * @param self The map scene
- * @param globalGameState The global game state
- * @param renderer The SDL renderer
- */
 void scene_map_destroy(Scene *self, GlobalGameState *globalGameState, SDL_Renderer *renderer);
